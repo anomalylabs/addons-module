f4e0e9c86087e8902935a91976302b5522448736
Ryan Thompson <service@aiwebsystems.com>
2014-08-23 22:28:23 -0500

    Fix namespaces

--- routes.php
+++ routes.php
@@ -9,26 +9,26 @@ Route::get(
 );
 
 // List all addons
-Route::get('admin/addons/modules', 'Addon\Module\Addons\Controller\Admin\ModulesController@index');
-Route::get('admin/addons/themes', 'Addon\Module\Addons\Controller\Admin\ThemesController@index');
-Route::get('admin/addons/blocks', 'Addon\Module\Addons\Controller\Admin\BlocksController@index');
-Route::get('admin/addons/extensions', 'Addon\Module\Addons\Controller\Admin\ExtensionsController@index');
-Route::get('admin/addons/field_types', 'Addon\Module\Addons\Controller\Admin\FieldTypesController@index');
-Route::get('admin/addons/tags', 'Addon\Module\Addons\Controller\Admin\TagsController@index');
+Route::get('admin/addons/modules', 'Streams\Addon\Module\Addons\Controller\Admin\ModulesController@index');
+Route::get('admin/addons/themes', 'Streams\Addon\Module\Addons\Controller\Admin\ThemesController@index');
+Route::get('admin/addons/blocks', 'Streams\Addon\Module\Addons\Controller\Admin\BlocksController@index');
+Route::get('admin/addons/extensions', 'Streams\Addon\Module\Addons\Controller\Admin\ExtensionsController@index');
+Route::get('admin/addons/field_types', 'Streams\Addon\Module\Addons\Controller\Admin\FieldTypesController@index');
+Route::get('admin/addons/tags', 'Streams\Addon\Module\Addons\Controller\Admin\TagsController@index');
 
 // Create
-Route::any('admin/addons/modules/create', 'Addon\Module\Addons\Controller\Admin\ModulesController@create');
+Route::any('admin/addons/modules/create', 'Streams\Addon\Module\Addons\Controller\Admin\ModulesController@create');
 
 // Edit
-Route::any('admin/addons/modules/edit/{id}', 'Addon\Module\Addons\Controller\Admin\ModulesController@edit');
+Route::any('admin/addons/modules/edit/{id}', 'Streams\Addon\Module\Addons\Controller\Admin\ModulesController@edit');
 
 // Install an addon
 Route::get(
     'admin/addons/installer/install/{type}/{slug}',
-    'Addon\Module\Addons\Controller\Admin\InstallerController@install'
+    'Streams\Addon\Module\Addons\Controller\Admin\InstallerController@install'
 );
 
 Route::get(
     'admin/addons/installer/uninstall/{type}/{slug}',
-    'Addon\Module\Addons\Controller\Admin\InstallerController@uninstall'
+    'Streams\Addon\Module\Addons\Controller\Admin\InstallerController@uninstall'
 );
