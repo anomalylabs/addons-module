0d38dd8b73cb197b7558053c0e6f9866c12c269c
Ryan Thompson <service@aiwebsystems.com>
2015-01-03 17:37:08 -0600

    Entries hadnler

--- src/Ui/Table/Module/Handler/EntriesHandler.php
+++ src/Ui/Table/Module/Handler/EntriesHandler.php
@@ -1,6 +1,7 @@
 <?php namespace Anomaly\AddonsModule\Ui\Table\Module\Handler;
 
 use Anomaly\Streams\Platform\Addon\Module\ModuleCollection;
+use Illuminate\Http\Request;
 
 /**
  * Class EntriesHandler
@@ -14,6 +15,23 @@ class EntriesHandler
 {
 
     /**
+     * The request object.
+     *
+     * @var \Illuminate\Http\Request
+     */
+    protected $request;
+
+    /**
+     * Create a new EntriesHandler instance.
+     *
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
      * Handle the table entries.
      *
      * @param ModuleCollection $modules
@@ -21,6 +39,26 @@ class EntriesHandler
      */
     public function handle(ModuleCollection $modules)
     {
-        return $modules->orderByName();
+
+        /**
+         * Since we are not using a query / model
+         * we will switch the collection scope
+         * based on the view here.
+         */
+        switch ($this->request->get('view')) {
+
+            // Installed
+            case 'installed':
+                return $modules->installed()->orderByName();
+                break;
+
+            // Uninstalled
+            case 'uninstalled':
+                return $modules->uninstalled()->orderByName();
+                break;
+
+            default:
+                return $modules->orderByname();
+        }
     }
 }
