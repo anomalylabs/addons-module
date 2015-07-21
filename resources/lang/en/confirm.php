d44c8a196cd6e3bd69b9912c91516b8840ecff73
Ryan Thompson <service@aiwebsystems.com>
2015-07-21 05:39:52 -0500

    Ready

---
+++ resources/lang/en/confirm.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'uninstall_core_addon' => 'Are you sure you want to uninstall this <strong>core</strong> addon?<br><small>This is a core addon and may cause your installation to become inoperable.</small>'
+];
