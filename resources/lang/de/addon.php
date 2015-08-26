53d114c03f6461167a6dca98c4d9a2698b9e309a
Marco Grüter <marco.grueter@healthworld.ch>
2015-08-26 11:30:35 +0200

    german translation

---
+++ resources/lang/de/addon.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title'       => 'Addons',
+    'name'        => 'Addons Modul',
+    'description' => 'Addons Verwaltung und Informationen.',
+    'section'     => [
+        'plugins'     => 'Plugins',
+        'themes'      => 'Themes',
+        'modules'     => 'Module',
+        'extensions'  => 'Erweiterungen',
+        'field_types' => 'Feldtypen'
+    ]
+];
