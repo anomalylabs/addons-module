267c1eb3f389a0cf1dcf958febce47efb5da1a57
Toby Evans <tobz.nz@gmail.com>
2015-12-01 16:49:03 +1300

    fix de language file

--- resources/lang/de/permission.php
+++ resources/lang/de/permission.php
@@ -2,35 +2,44 @@
 
 return [
     'modules'     => [
-        'title'     => 'Module',
-        'read'      => 'Kann Module einsehen?',
-        'install'   => 'Kann Module installieren?',
-        'uninstall' => 'Kann Module deinstallieren?',
-        'disable'   => 'Kann Module deaktivieren?',
-        'delete'    => 'Kann Module löschen?'
+        'name'     => 'Module',
+        'option' => [
+            'read'      => 'Kann Module einsehen?',
+            'install'   => 'Kann Module installieren?',
+            'uninstall' => 'Kann Module deinstallieren?',
+            'disable'   => 'Kann Module deaktivieren?',
+            'delete'    => 'Kann Module löschen?'
+        ]
     ],
     'themes'      => [
-        'title'  => 'Themes',
-        'read'   => 'Kann Themes einsehen?',
-        'delete' => 'Kann Themes löschen?'
+        'name'  => 'Themes',
+        'option' => [
+            'read'   => 'Kann Themes einsehen?',
+            'delete' => 'Kann Themes löschen?'
+        ]
     ],
     'plugins'     => [
-        'title'  => 'Plugins',
-        'read'   => 'Kann Plugins einsehen?',
-        'delete' => 'Kann Plugins löschen?'
+        'name'  => 'Plugins',
+        'option' => [
+            'read'   => 'Kann Plugins einsehen?',
+            'delete' => 'Kann Plugins löschen?'
+        ]
     ],
     'extensions'  => [
-        'title'     => 'Erweiterungen',
-        'read'      => 'Kann Erweiterungen einsehen?',
-        'install'   => 'Kann Erweiterungen installieren?',
-        'uninstall' => 'Kann Erweiterungen deinstallieren?',
-        'disable'   => 'Kann Erweiterungen deaktivieren?',
-        'delete'    => 'Kann Erweiterungen löschen?'
+        'name'     => 'Erweiterungen',
+        'option' => [
+            'read'      => 'Kann Erweiterungen einsehen?',
+            'install'   => 'Kann Erweiterungen installieren?',
+            'uninstall' => 'Kann Erweiterungen deinstallieren?',
+            'disable'   => 'Kann Erweiterungen deaktivieren?',
+            'delete'    => 'Kann Erweiterungen löschen?'
+        ]
     ],
     'field_types' => [
-        'title'  => 'Feldtypen',
-        'read'   => 'Kann Feldtypen einsehen?',
-        'delete' => 'Kann Feldtypen löschen?'
+        'name'  => 'Feldtypen',
+        'option' => [
+            'read'   => 'Kann Feldtypen einsehen?',
+            'delete' => 'Kann Feldtypen löschen?'
+        ]
     ]
 ];
-
