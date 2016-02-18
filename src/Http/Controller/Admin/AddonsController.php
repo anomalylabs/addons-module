0944f9a16a1bae1849cb9001eb95c3694bde5e3d
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 01:49:44 -0600

    Author

--- src/Http/Controller/Admin/AddonsController.php
+++ src/Http/Controller/Admin/AddonsController.php
@@ -14,9 +14,9 @@ use Illuminate\Http\Request;
 /**
  * Class AddonsController
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\AddonsModule\Http\Controller\Admin
  */
 class AddonsController extends AdminController
