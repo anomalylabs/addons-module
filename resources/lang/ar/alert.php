1d6a3f17c03e40869d134678eb375c6605948ddd
aladinfajr <aladinfajr@gmail.com>
2016-09-10 12:20:38 +0300

    translate addons module to arabic

---
+++ resources/lang/ar/alert.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'install_module_success'      => 'لقد تم تثبيت وحدة :module بنجاح',
+    'uninstall_module_success'    => 'لقد تم إزالة وحدة :module !',
+    'enable_module_success'       => 'لقد تم تفعيل وحدة :module !',
+    'disable_module_success'      => 'لقد تم تعطيل وحدة :module !',
+    'install_extension_success'   => 'لقد تم تثبيت الملحق :extension بنجاح!',
+    'uninstall_extension_success' => 'لقد تم إزالة الملحق :extension !',
+    'enable_extension_success'    => 'لقد تم تفعيل الملحق :extension !',
+    'disable_extension_success'   => 'لقد تم تعطيل الملحق :extension !',
+    'addon_delete_success'        => 'لقد تم حذف الإضافة :addon من المخدم!',
+    'addon_delete_error'          => 'لايمكن حذف الإضافة :addon من المخدم!',
+];
---
+++ resources/lang/ar/label.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'link'          => 'رابط',
+    'type'          => 'نوع',
+    'path'          => 'مسار',
+    'docs'          => 'مستند',
+    'authors'       => 'منشأ',
+    'support'       => 'دعم',
+    'version'       => 'إضدار',
+    'standard'      => 'قياسي',
+    'information'   => 'معلومات',
+    'irc_channel'   => 'IRC قناة',
+    'known_issues'  => 'المشكللات المعروفة',
+    'project_wiki'  => 'مشروع ويكي',
+    'not_installed' => 'غير مثبت',
+    'support_email' => 'بريد اللكتروني للدعم',
+    'support_forum' => 'منتدى الدعم',
+];
