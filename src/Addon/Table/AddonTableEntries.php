5c8ddccdd717d39c17e30b62e7a2573aaabaad22
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-23 05:50:48 +0300

    Update AddonTableEntries.php

--- src/Addon/Table/AddonTableEntries.php
+++ src/Addon/Table/AddonTableEntries.php
@@ -1,17 +1,23 @@
 <?php namespace Anomaly\AddonsModule\Addon\Table;
 
+use Anomaly\AddonsModule\Addon\Table\Command\FetchAddons;
+use Anomaly\AddonsModule\Addon\Table\Command\GetAddonData;
+use Anomaly\Streams\Platform\Addon\Addon;
 use Anomaly\Streams\Platform\Addon\AddonCollection;
-
+use Illuminate\Foundation\Bus\DispatchesJobs;
 
 /**
  * Class AddonTableEntries
  *
- * @link          http://pyrocms.com/
  * @author        PyroCMS, Inc. <support@pyrocms.com>
  * @author        Ryan Thompson <ryan@pyrocms.com>
+ *
+ * @link          http://pyrocms.com/
  */
 class AddonTableEntries
 {
+
+    use DispatchesJobs;
 
     /**
      * Handle the command.
@@ -21,6 +27,31 @@ class AddonTableEntries
      */
     public function handle(AddonTableBuilder $builder, AddonCollection $addons)
     {
+        if (array_get($_GET, 'view') == 'github')
+        {
+            $items = new AddonCollection([]);
+            $repos = $this->dispatch(new FetchAddons($addons));
+
+            foreach ($repos as $repo)
+            {
+                $data = $this->dispatch(new GetAddonData($repo));
+
+                if (!$addons->get(array_get($data, 'namespace'), false))
+                {
+                    $addon = (new Addon())
+                    ->setVendor(array_get($data, 'vendor'))
+                    ->setType(array_get($data, 'type'))
+                    ->setSlug(array_get($data, 'slug'));
+
+                    $items->put($addon->getNamespace(), $addon);
+                }
+            }
+
+            $builder->setTableEntries($items);
+
+            return;
+        }
+
         $addons = $addons->{$builder->getType()}();
 
         $builder->setTableEntries($addons);
