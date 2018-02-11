a0dad974ec51000bc552142ed2e85d8711cb35b9
Ryan Thompson <ryan@pyrocms.com>
2018-02-11 13:10:41 -0600

    Done

--- resources/config/repository.php
+++ resources/config/repository.php
@@ -13,4 +13,10 @@ return [
         'title'       => 'anomaly.module.addons::repository.community.title',
         'description' => 'anomaly.module.addons::repository.community.description',
     ],
+//    'development' => [
+//        'url'         => 'https://development.pyrocms.com',
+//        'name'        => 'anomaly.module.addons::repository.development.name',
+//        'title'       => 'anomaly.module.addons::repository.development.title',
+//        'description' => 'anomaly.module.addons::repository.development.description',
+//    ],
 ];
--- resources/lang/en/repository.php
+++ resources/lang/en/repository.php
@@ -1,19 +1,24 @@
 <?php
 
 return [
-    'all'       => [
+    'all'         => [
         'name'        => 'All',
         'title'       => 'All Addons',
         'description' => 'All available addons from all configured repositories.',
     ],
-    'featured'  => [
+    'featured'    => [
         'name'        => 'Featured',
         'title'       => 'Featured Addons',
         'description' => 'First-party addons from the makers of Pyro. Unlimited licensing for <span class="tag tag-danger">PRO</span> addons require a <a href="https://pyrocms.com/pro" target="_blank">PRO subscription</a>.',
     ],
-    'community' => [
+    'community'   => [
         'name'        => 'Community',
         'title'       => 'Community Addons',
         'description' => 'Third-party addons contributed by the community. <a href="https://github.com/pyrocms/community.pyrocms.com/blob/master/satis.json" target="_blank">Add yours to the list.</a>',
+    ],
+    'development' => [
+        'name'        => 'Development',
+        'title'       => 'Active Development',
+        'description' => 'Addons from the makers of Pyro that are in active development.',
     ],
 ];
