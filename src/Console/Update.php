3cff422df32c4f57e77a685e19dfef56b26f1e11
Ryan Thompson <service@aiwebsystems.com>
2019-01-31 13:18:44 -0600

    Update too

--- src/Console/Update.php
+++ src/Console/Update.php
@@ -43,17 +43,16 @@ class Update extends Command
             return;
         }
 
-        $process = ComposerProcess::make(
-            'update',
-            join(
-                ' ',
-                [
-                    $addon->getName(),
-                    '--optimize-autoloader',
-                    //'--update-no-dev',
-                ]
-            )
-        );
+        $parameters = [
+            $addon->getName(),
+            '--optimize-autoloader',
+        ];
+
+        if (env('APP_ENV') == 'production') {
+            $parameters[] = '--update-no-dev';
+        }
+
+        $process = ComposerProcess::make('update', join(' ', $parameters));
 
         dispatch_now(new RunProcess($this, $process));
         dispatch_now(new FinishUpdate($this, $addon));
