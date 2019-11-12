1407d92e833cdad2ff9489697966f83bb92772e5
Igor <igor.volkov@maxi-booking.ru>
2019-11-12 15:52:44 +0300

    add russian translations

---
+++ resources/lang/ru/message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'install_addon_success'   => 'Дополнение успешно установлено',
+    'uninstall_addon_success' => 'Дополнение успешно деинсталлировано',
+    'migrate_addon_success'   => 'Дополнение успешно перенесено',
+    'disable_addon_success'   => 'Дополнение успешно отключено',
+    'enable_addon_success'    => 'Дополнение успешно включено',
+    'downloading'             => 'Загрузка дополнения...',
+    'removing'                => 'Удаление дополнения...',
+    'updating'                => 'Обновление дополнения...',
+];
